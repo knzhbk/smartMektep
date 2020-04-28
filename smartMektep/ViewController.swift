aec7b0f8615d4c128063dcefd006ba72fd4dbe6e
Adina Kenzhebek <adina18022013@gmail.com>
2020-04-29 02:39:18 +0600

    Initial Commit

---
+++ smartMektep/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  smartMektep
+//
+//  Created by Adina Kenzhebekova on 4/29/20.
+//  Copyright © 2020 Adina Kenzhebekova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
